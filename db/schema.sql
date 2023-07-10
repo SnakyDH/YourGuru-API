165b184f3c2065985a4719d2f616b58d28feed93
SnakyDH <skdanielg5@gmail.com>
2023-07-09 20:11:30 -0500

    feat: update db

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,13 @@
--- Database generated with pgModeler (PostgreSQL Database Modeler).
--- pgModeler version: 0.9.4
--- PostgreSQL version: 13.0
--- Project Site: pgmodeler.io
--- Model Author: ---
-
--- Database creation must be performed outside a multi lined SQL file.
--- These commands were put in this file only as a convenience.
---
--- object: new_database | type: DATABASE --
--- DROP DATABASE IF EXISTS new_database;
---CREATE DATABASE new_database;
--- ddl-end --
 
 
 -- object: public."user" | type: TABLE --
 -- DROP TABLE IF EXISTS public."user" CASCADE;
 CREATE TABLE public."user" (
-                               id varchar(20) NOT NULL,
+                               id serial NOT NULL,
                                username varchar(20) NOT NULL,
                                email varchar(70) NOT NULL,
                                password varchar(100) NOT NULL,
-                               id_rol_user smallint,
+                               id_user_rol smallint,
                                CONSTRAINT user_pk PRIMARY KEY (id)
 );
 -- ddl-end --
@@ -34,29 +21,30 @@ CREATE TABLE public.item (
                              release_date timestamp,
                              created_at timestamp NOT NULL,
                              updated_at timestamp NOT NULL,
-                             id_user varchar(20),
-                             id_type_item integer,
+                             id_user integer,
                              id_description integer,
+                             id_item_type integer,
+                             id_item_state smallint,
                              CONSTRAINT item_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 -- ddl-end --
 
--- object: public.opinion | type: TABLE --
--- DROP TABLE IF EXISTS public.opinion CASCADE;
-CREATE TABLE public.opinion (
-                                id serial NOT NULL,
-                                info varchar(150) NOT NULL,
-                                rating smallint NOT NULL,
-                                id_item integer,
-                                CONSTRAINT opinion_pk PRIMARY KEY (id)
+-- object: public.review | type: TABLE --
+-- DROP TABLE IF EXISTS public.review CASCADE;
+CREATE TABLE public.review (
+                               id serial NOT NULL,
+                               comment varchar(300) NOT NULL,
+                               rating smallint NOT NULL,
+                               id_item integer,
+                               CONSTRAINT opinion_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 -- ddl-end --
 
--- object: public.rol_user | type: TABLE --
--- DROP TABLE IF EXISTS public.rol_user CASCADE;
-CREATE TABLE public.rol_user (
+-- object: public.user_rol | type: TABLE --
+-- DROP TABLE IF EXISTS public.user_rol CASCADE;
+CREATE TABLE public.user_rol (
                                  id smallserial NOT NULL,
                                  rol varchar(10) NOT NULL,
                                  CONSTRAINT type_user_pk PRIMARY KEY (id)
@@ -64,12 +52,12 @@ CREATE TABLE public.rol_user (
 -- ddl-end --
 -- ddl-end --
 
--- object: public.type_item | type: TABLE --
--- DROP TABLE IF EXISTS public.type_item CASCADE;
-CREATE TABLE public.type_item (
+-- object: public.item_type | type: TABLE --
+-- DROP TABLE IF EXISTS public.item_type CASCADE;
+CREATE TABLE public.item_type (
                                   id serial NOT NULL,
                                   type varchar(40) NOT NULL,
-                                  CONSTRAINT actors_pk PRIMARY KEY (id)
+                                  CONSTRAINT type_item_pk PRIMARY KEY (id)
 );
 -- ddl-end --
 -- ddl-end --
@@ -85,8 +73,8 @@ CREATE TABLE public.description (
 -- ddl-end --
 
 -- object: item_fk | type: CONSTRAINT --
--- ALTER TABLE public.opinion DROP CONSTRAINT IF EXISTS item_fk CASCADE;
-ALTER TABLE public.opinion ADD CONSTRAINT item_fk FOREIGN KEY (id_item)
+-- ALTER TABLE public.review DROP CONSTRAINT IF EXISTS item_fk CASCADE;
+ALTER TABLE public.review ADD CONSTRAINT item_fk FOREIGN KEY (id_item)
     REFERENCES public.item (id) MATCH FULL
     ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
@@ -98,23 +86,11 @@ ALTER TABLE public.item ADD CONSTRAINT user_fk FOREIGN KEY (id_user)
     ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
--- object: type_item_fk | type: CONSTRAINT --
--- ALTER TABLE public.item DROP CONSTRAINT IF EXISTS type_item_fk CASCADE;
-ALTER TABLE public.item ADD CONSTRAINT type_item_fk FOREIGN KEY (id_type_item)
-    REFERENCES public.type_item (id) MATCH FULL
+-- object: item_type_fk | type: CONSTRAINT --
+-- ALTER TABLE public.item DROP CONSTRAINT IF EXISTS item_type_fk CASCADE;
+ALTER TABLE public.item ADD CONSTRAINT item_type_fk FOREIGN KEY (id_item_type)
+    REFERENCES public.item_type (id) MATCH FULL
     ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
--- object: rol_user_fk | type: CONSTRAINT --
--- ALTER TABLE public."user" DROP CONSTRAINT IF EXISTS rol_user_fk CASCADE;
-ALTER TABLE public."user" ADD CONSTRAINT rol_user_fk FOREIGN KEY (id_rol_user)
-    REFERENCES public.rol_user (id) MATCH FULL
-    ON DELETE SET NULL ON UPDATE CASCADE;
--- ddl-end --
-
--- object: user_uq | type: CONSTRAINT --
--- ALTER TABLE public."user" DROP CONSTRAINT IF EXISTS user_uq CASCADE;
-ALTER TABLE public."user" ADD CONSTRAINT user_uq UNIQUE (id_rol_user);
 -- ddl-end --
 
 -- object: description_fk | type: CONSTRAINT --
@@ -160,6 +136,30 @@ CREATE TABLE public.genre (
 -- ALTER TABLE public.item_genre DROP CONSTRAINT IF EXISTS genre_fk CASCADE;
 ALTER TABLE public.item_genre ADD CONSTRAINT genre_fk FOREIGN KEY (id_genre)
     REFERENCES public.genre (id) MATCH FULL
+    ON DELETE SET NULL ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: public.item_state | type: TABLE --
+-- DROP TABLE IF EXISTS public.item_state CASCADE;
+CREATE TABLE public.item_state (
+                                   id smallserial NOT NULL,
+                                   state varchar(10) DEFAULT 'to do',
+                                   CONSTRAINT item_state_pk PRIMARY KEY (id)
+);
+-- ddl-end --
+-- ddl-end --
+
+-- object: item_state_fk | type: CONSTRAINT --
+-- ALTER TABLE public.item DROP CONSTRAINT IF EXISTS item_state_fk CASCADE;
+ALTER TABLE public.item ADD CONSTRAINT item_state_fk FOREIGN KEY (id_item_state)
+    REFERENCES public.item_state (id) MATCH FULL
+    ON DELETE SET NULL ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: user_rol_fk | type: CONSTRAINT --
+-- ALTER TABLE public."user" DROP CONSTRAINT IF EXISTS user_rol_fk CASCADE;
+ALTER TABLE public."user" ADD CONSTRAINT user_rol_fk FOREIGN KEY (id_user_rol)
+    REFERENCES public.user_rol (id) MATCH FULL
     ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
